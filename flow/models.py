9909970bdbfc23e9a0eda180b18376b779989839
Beko1771 <nobexruz@gmail.com>
2024-12-27 15:35:11 +0500

    loyihaga modellar qoshildi

--- flow/models.py
+++ flow/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class flow(models.Model):
+    title = models.CharField(max_length=20)
+    description = models.TextField()
+    image = models.ImageField(upload_to='post/')
+    create_at = models.DateTimeField(auto_now_add=True)
